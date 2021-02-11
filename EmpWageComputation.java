8aa53dc7dd86f2ec37c67362daa353780fa817f1
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-11 15:29:49 +0530

    UC2-Calculate Full Time Empolyee Daily wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,16 +1,21 @@
 public class EmpWageComputation
 {
+	static final int isFullTime=1;
+        	static final int EmpRatePerHour=20;
+
+
 	public static void main(String args [])
 	{
-		int is_Full_Time = 1;
 
-		double empCheck = Math.floor(Math.random() *10 % 2);
+		int EmpHrs=0;
+		int EmpWage = 0;
 
-		if(empCheck == is_Full_Time)
-		{
-			System.out.println("Employee is Present");
-		}
-		else
-			System.out.println("Employee is Absent");
+		double EmpCheck = Math.floor(Math.random() *10) % 2 ;
+
+		if(EmpCheck == isFullTime)
+			EmpHrs=8;
+
+		EmpWage =EmpHrs * EmpRatePerHour;
+		System.out.println("Emp Wage :-"+EmpWage);
 	}
 }
