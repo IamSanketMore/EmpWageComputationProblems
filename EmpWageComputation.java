55bdc656285d9d4ab34a91f1b9131731b84a963b
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-11 15:38:42 +0530

    UC3-Calculate DailyWage Full-Time & Part-Time

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,21 +1,33 @@
 public class EmpWageComputation
 {
-	static final int isFullTime=1;
-        	static final int EmpRatePerHour=20;
+        static final int isFullTime=1;
+        static final int EmpRatePerHour=20;
 
 
-	public static void main(String args [])
+        public static void main(String args [])
+        {
+		int isPartTime=2;
+
+                int EmpHrs=0;
+                int EmpWage = 0;
+
+                double EmpCheck = Math.floor(Math.random() *10) % 3 ;
+
+                  if(EmpCheck == isFullTime)
 	{
-
-		int EmpHrs=0;
-		int EmpWage = 0;
-
-		double EmpCheck = Math.floor(Math.random() *10) % 2 ;
-
-		if(EmpCheck == isFullTime)
-			EmpHrs=8;
-
-		EmpWage =EmpHrs * EmpRatePerHour;
-		System.out.println("Emp Wage :-"+EmpWage);
+		System.out.println("Employee is Full-Time");
+		EmpHrs=8;
+	}	
+	else if (EmpCheck == isPartTime)
+	{
+		System.out.println("Employee is Part-Time");
+		EmpHrs=4;
 	}
+	else
+	{
+		System.out.println("Employee is Absent");
+	}
+                EmpWage =EmpHrs * EmpRatePerHour;
+                System.out.println("Emp Wage :-"+EmpWage);
+        }
 }
