21b335602791e215984d195846edc64d9645af4f
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-11 16:00:03 +0530

    UC-5 Calculating Employee Wage per Month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,35 +2,44 @@ public class EmpWageComputation
 {
         public static final int isFullTime=1;
         public static final int EmpRatePerHour=20;
-	public static final int isPartTime=2;
-
+        public static final int isPartTime=2;
+	public static final int NumOfWorkingDays=2;
 
         public static void main(String args [])
         {
 
                 int EmpHrs=0;
                 int EmpWage = 0;
+		int TotalEmpWage=0;
 
-                double EmpCheck = Math.floor(Math.random() *10) % 3 ;
+               // double EmpCheck = (int)Math.floor(Math.random() *10) % 3 ;
 
-	switch ((int)EmpCheck)
-	{
-               	case isFullTime :
-		System.out.println("Employee is Full-Time");
-                        	EmpHrs=8;
-		break;
+		for(int day = 0; day < NumOfWorkingDays; day++ )
+		{
+			int EmpCheck = (int)Math.floor(Math.random() *10) % 3 ;
 
-                	case isPartTime :
-		System.out.println("Employee is Part-Time");
-                        	EmpHrs=4;
-		break;
+                	switch (EmpCheck)
+                	{
+                        	case isFullTime :
+					System.out.println("Employee is Full-Time");
+                        	        EmpHrs=8;
+                        	        break;
 
-                	default :
-                        	System.out.println("Employee is Absent");
-		break;
+                       		 case isPartTime :
+					System.out.println("Employee is Part-Time");
+                                	EmpHrs=4;
+                                	break;
 
-	}
+                        	default :
+                                	System.out.println("Employee is Absent");
+					EmpHrs=0;
+                                	break;
+
+                	}
                 EmpWage =EmpHrs * EmpRatePerHour;
-                System.out.println("Emp Wage :-"+EmpWage);
-        }
+                TotalEmpWage += EmpWage;
+		System.out.println("Emp Wage :-"+EmpWage);
+        	}
+		System.out.println("Total EmpWage:" + TotalEmpWage);
+	}
 }
