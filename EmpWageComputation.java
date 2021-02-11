75d45f014e54e7e821ea07d664d07e19fffc604b
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-11 15:48:15 +0530

    UC4-Solve Daily Wage using Switch case Statement

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,31 +1,34 @@
 public class EmpWageComputation
 {
-        static final int isFullTime=1;
-        static final int EmpRatePerHour=20;
+        public static final int isFullTime=1;
+        public static final int EmpRatePerHour=20;
+	public static final int isPartTime=2;
 
 
         public static void main(String args [])
         {
-		int isPartTime=2;
 
                 int EmpHrs=0;
                 int EmpWage = 0;
 
                 double EmpCheck = Math.floor(Math.random() *10) % 3 ;
 
-                  if(EmpCheck == isFullTime)
+	switch ((int)EmpCheck)
 	{
+               	case isFullTime :
 		System.out.println("Employee is Full-Time");
-		EmpHrs=8;
-	}	
-	else if (EmpCheck == isPartTime)
-	{
+                        	EmpHrs=8;
+		break;
+
+                	case isPartTime :
 		System.out.println("Employee is Part-Time");
-		EmpHrs=4;
-	}
-	else
-	{
-		System.out.println("Employee is Absent");
+                        	EmpHrs=4;
+		break;
+
+                	default :
+                        	System.out.println("Employee is Absent");
+		break;
+
 	}
                 EmpWage =EmpHrs * EmpRatePerHour;
                 System.out.println("Emp Wage :-"+EmpWage);
